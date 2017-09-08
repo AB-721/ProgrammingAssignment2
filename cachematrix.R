02157678aab46d436254c99187bcb3e9f1743a5b
Keen_Explorer <31710753+AB-721@users.noreply.github.com>
2017-09-08 12:14:50 -0700

    Assignment Commit
    Matrix Inverse File

--- cachematrix.R
+++ cachematrix.R
@@ -1,15 +1,55 @@
-## Put comments here that give an overall description of what your
-## functions do
 
-## Write a short comment describing this function
+
+## This file has two functions "makeCacheMatrix" & "cacheSolve"
+## makeCacheMatrix function accepts a matrix and returns a list of functions. It aims to cache the calculated values of the inverse of a matrix
+## cacheSolve function checks to see if a cached inverse is already calculated for a matrix.
+## If not, it checks to see if the matrix in question in question is a square matrix and Non-singular [Non zero det]
+## If both the above conditions are satisfied, it calculates the inverse of a matrix using solve() functions & sets the inverse
 
 makeCacheMatrix <- function(x = matrix()) {
-
+	inverse <- NULL
+	set <- function(y)
+		{
+		x <<- y
+		inverse <<- NULL
+		}
+	get <-function()x
+	setinverse <- function (minverse)
+		{
+		inverse <<- minverse
+		}
+	getinverse<-function()inverse
+	list(set=set, get=get, setinverse=setinverse,getinverse=getinverse)
 }
 
 
-## Write a short comment describing this function
+
 
 cacheSolve <- function(x, ...) {
-        ## Return a matrix that is the inverse of 'x'
+## Return a matrix that is the inverse of 'x'
+
+ m <- x$getinverse()
+        if(!is.null(m))
+		{
+                message("getting cached data")
+                return(m)
+        }
+        data <- x$get()
+##Check whether data is a square matrix
+	  if(nrow(data) != ncol(data))
+	  {
+         message ("A non-square matrix was passed and it cannot have an inverse")		
+         return()
+	  }	
+##Check whether data is a non-singular matrix i.e matrix determinant should be non zero for inverse to exist
+	  if (det(data)==0)
+	  {
+         message ("A Singular matrix was passed and it cannot have an inverse")		
+         return()
+	  }	
+##Use the Solve function to calculate matrix inverse
+	  m <- solve(data, ...)
+        x$setinverse(m)
+        m
+
 }
